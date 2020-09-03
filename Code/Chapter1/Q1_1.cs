fc985de5c2d0d6261c945ad65ee493aecfc3eb24
Fraser Douglas <fraser.j.douglas@gmail.com>
2020-09-03 22:03:51 +0100

    adds chapter 1, q1 code and tests

---
+++ Code/Chapter1/Q1_1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.Chapter1
+{
+    public static class Q1_1
+    {
+        // 1.1 Is Unique: Implement an algorithm to determine if a string has all unique characters.What if you
+        //cannot use additional data structures?
+        //Hints: #44, # 777, # 7 32 
+        
+        public static bool areAllCharsUnique(string inputString)
+        {
+            if (String.IsNullOrEmpty(inputString))
+            {
+                return true;
+            }
+
+            HashSet<char> characterHash = new HashSet<char>();
+
+            foreach (char character in inputString)
+            {
+                if (!characterHash.Add(character))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool areAllCharsUniqueNoAdditional(string inputString)
+        {
+            //cannot use additional data structures so loop through
+            //each letter and compare it with the remaining letters
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                for (int j = i + 1; j < inputString.Length; j++)
+                {
+                    if (inputString[i] == inputString[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+
+
+    }
+}
--- UnitTests/Chapter1/C1Q1_Tests.cs
+++ UnitTests/Chapter1/C1Q1_Tests.cs
@@ -1,13 +1,55 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Code.Chapter1;
 
 namespace UnitTests
 {
     [TestClass]
     public class C1Q1_Tests
     {
+        //arrange
+        private string _uniqueInput = "abcdefghijklmnopqrstuvwxyz";
+        private string _duplicateInput = "aabcdefghijklmnopqrstuvwxyz";
+
         [TestMethod]
-        public void TestMethod1()
+        public void ReturnsTrueIfUnique()
         {
+            //act
+            bool result = Q1_1.areAllCharsUnique(_uniqueInput);
+
+            //assert
+            Assert.IsTrue(result);
         }
+
+        [TestMethod]
+        public void  ReturnsFalseIfNotUnique()
+        {            
+            //act
+            bool result = Q1_1.areAllCharsUnique(_duplicateInput);
+
+            //assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ReturnTrueIfUniqeNoAdditional()
+        {
+            //act
+            bool result = Q1_1.areAllCharsUniqueNoAdditional(_uniqueInput);
+
+            //assert
+            Assert.IsTrue(result);
+        }
+
+
+        [TestMethod]
+        public void ReturnFalseIfNoUniqeNoAdditional()
+        {
+            //act
+            bool result = Q1_1.areAllCharsUniqueNoAdditional(_duplicateInput);
+
+            //assert
+            Assert.IsFalse(result);
+        }
+
     }
 }
