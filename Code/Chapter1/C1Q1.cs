c3c7134d58ee879b66eb98016541015d7cda2880
Fraser Douglas <fraser.j.douglas@gmail.com>
2020-09-03 21:08:22 +0100

    adds code project

---
+++ Code/Chapter1/C1Q1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.Chapter1
+{
+    class C1Q1
+    {
+    }
+}
