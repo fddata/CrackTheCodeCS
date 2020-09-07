bef40bbaf91ad31ad64d7005e13e09e1759f757e
Fraser Douglas <fraser.j.douglas@gmail.com>
2020-09-07 20:54:06 +0100

    adds Q1_3

---
+++ Code/Chapter1/Q1_3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.Chapter1
+{
+    public static class Q1_3
+    {
+        //URLify: Write a method to replace all spaces in a string with '%20: You may assume that the string
+        //has sufficient space at the end to hold the additional characters, and that you are given the "true"
+        //length of the string. (Note: If implementing in Java, please use a character array so that you can
+        //perform this operation in place.)
+        //Hints: #53, #118
+
+        public static string ConvertStringSpacesToHTML(char[] chars, int length)
+        {
+            return "hjfghfg";
+
+
+        }
+
+
+    }
+}
