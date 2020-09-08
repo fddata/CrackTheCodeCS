bacd340c0b61bded8ae37d1edb74c5c583453b5c
Fraser Douglas <fraser.j.douglas@gmail.com>
2020-09-08 22:10:35 +0100

    adds c1q4 and tests

---
+++ Code/Chapter1/Q1_4.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.Chapter1
+{
+    public static class Q1_4
+    {
+        //Palindrome Permutation: Given a string, write a function to check if it is a permutation of a palindrome.
+        //A palindrome is a word or phrase that is the same forwards and backwards.A permutation 
+        //is a rearrangement of letters.The palindrome does not need to be limited to just dictionary words.
+        //EXAMPLE
+        //Input: Tact Coa
+        //Output: True (permutations: "taco cat". "atco cta". etc.)
+        //Hints: #106, #121, #134, #136 
+
+
+        //palindrome must have only 1 or zero characters with an odd number 
+
+        public static bool IsStringAPalindromePermutation(string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException( nameof(input), "Argument cannot be null");
+            }
+
+            input = input.Replace(" ", String.Empty);
+            
+
+            var dict = new Dictionary<char, int>();
+
+            foreach (char character in input)
+            {
+                if (dict.ContainsKey(character))
+                {
+                    dict[character]++;
+                }
+                else
+                {
+                    dict.Add(character, 1);
+                }
+            }
+
+            bool foundOdd = false;
+
+            foreach (KeyValuePair<char, int> entry in dict)
+            {
+                if (entry.Value % 2 != 0)
+                {
+                    if (foundOdd)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        foundOdd = true;
+                    }
+                }
+
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Uses an alternating bool to record odd or even number of character occurrences (will be faster 
+        /// than performing equals check on a (potentially) large integer as in example above
+        /// </summary>
+        public static bool IsStringAPalindromePermutationV2(string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input), "Argument cannot be null");
+            }
+
+            input = input.Replace(" ", String.Empty);
+
+            var charOccurances = new Dictionary<char, bool>();
+
+            foreach (char character in input)
+            {
+                bool value;
+
+                charOccurances.TryGetValue(character, out value);
+
+                charOccurances[character] = (!value);
+            }
+
+            bool foundOdd = false;
+
+            foreach (KeyValuePair<char, bool> entry in charOccurances)
+            {
+                if (entry.Value) //if its been flagged as odd number of occurrences above
+                {
+                    if (foundOdd)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        foundOdd = true;
+                    }
+                }
+
+            }
+
+            return true;
+        }
+
+    }
+
+}
---
+++ UnitTests/Chapter1/C1Q4_Tests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Code.Chapter1;
+
+namespace UnitTests.Chapter1
+{
+    [TestClass]
+    public class C1Q4_Tests
+    {
+        private string _permutation = "akiviksaippiaspuaup"; //Pemutation of 'saippuakivikauppias' - the Finnish word for 'soapstone vendor' and supposedly the longest palindrome in everyday use:
+        private string _notPermutation = "not a permutation";
+
+        [TestMethod]
+        public void PermutationReturnsTrue()
+        {
+
+            //act
+            bool result = Q1_4.IsStringAPalindromePermutation(_permutation);
+
+            //assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void NonPermutationReturnsFalse()
+        {
+            //arrange
+            
+
+            //act
+            bool result = Q1_4.IsStringAPalindromePermutation(_notPermutation);
+
+            //assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void PermutationReturnsTrueV2()
+        {
+            //arrange
+            
+
+            //act
+            bool result = Q1_4.IsStringAPalindromePermutationV2(_permutation);
+
+            //assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void NonPermutationReturnsFalseV2()
+        {
+            //act
+            bool result = Q1_4.IsStringAPalindromePermutationV2(_notPermutation);
+
+            //assert
+            Assert.IsFalse(result);
+        }
+
+    }
+}
